fd3a32f1b2dd55bb5156639608fb81059eacda10
Eric Vallee <eric_vallee@webificservices.com>
2021-04-01 22:26:45 -0400

    fix(CQDG): #258 Indicated input and output variables that should remain hidden in the logs

--- output.tf
+++ output.tf
@@ -1,9 +1,11 @@
 output key {
-  value = tls_private_key.key.private_key_pem
+  value     = tls_private_key.key.private_key_pem
+  sensitive = true
 }
 
 output key_algorithm {
-  value = tls_private_key.key.algorithm
+  value     = tls_private_key.key.algorithm
+  sensitive = true
 }
 
 output certificate {
--- variables.tf
+++ variables.tf
@@ -1,28 +1,29 @@
 variable "organization" {
   description = "The etcd client's certificates' organization"
-  type = string
-  default = "Ferlab"
+  type        = string
+  default     = "Ferlab"
 }
 
 variable "username" {
   description = "The user the certificate is for"
-  type = string
-  default = "root"
+  type        = string
+  default     = "root"
 }
 
 variable "validity_period" {
   description = "Certificate's validity period in hours"
-  type = number
-  default = 100*365*24
+  type        = number
+  default     = 100*365*24
 }
 
 variable "early_renewal_period" {
   description = "Certificate's early renewal period in hours"
-  type = number
-  default = 99*365*24
+  type        = number
+  default     = 99*365*24
 }
 
 variable "ca" {
   description = "The ca that will sign the member's certificate. Should have the following keys: key, key_algorithm, certificate"
-  type = any
+  type        = any
+  sensitive   = true
 }
